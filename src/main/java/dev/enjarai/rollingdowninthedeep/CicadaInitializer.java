7a6dba10bf52cfacc6ba1df4f44ec85394ea1a2e
enjarai <enjarai123@gmail.com>
2024-05-09 12:42:03 +0200

    Nice

---
+++ src/main/java/dev/enjarai/rollingdowninthedeep/CicadaInitializer.java
@@ -0,0 +1,17 @@
+package dev.enjarai.rollingdowninthedeep;
+
+import nl.enjarai.cicada.api.conversation.ConversationManager;
+import nl.enjarai.cicada.api.util.CicadaEntrypoint;
+import nl.enjarai.cicada.api.util.JsonSource;
+import nl.enjarai.doabarrelroll.DoABarrelRoll;
+
+public class CicadaInitializer implements CicadaEntrypoint {
+    @Override
+    public void registerConversations(ConversationManager conversationManager) {
+        conversationManager.registerSource(
+                JsonSource.fromUrl("https://raw.githubusercontent.com/enjarai/rolling-down-in-the-deep/master/src/main/resources/cicada/rolling_down_in_the_deep/conversations.json")
+                        .or(JsonSource.fromResource("cicada/rolling_down_in_the_deep/conversations.json")),
+                RollingDownInTheDeep.LOGGER::info
+        );
+    }
+}
